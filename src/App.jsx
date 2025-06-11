4eed04940ffc975eab7c597a8db36a7e132e18d4
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2025-06-11 10:43:44 -0300

    Adição dos arquivos

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
